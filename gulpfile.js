d2f05e4e7281f34afd38f9108b7202b14124f752
mahy <dsz9258@163.com>
2019-07-09 00:24:10 +0800

    Feat: 逻辑重构

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,25 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 
 gulp.task('webpack', () => {
-    const webpack = require('webpack-stream')
-    const config = require('./webpack.config.js')
-    gulp.src('./src/js/**/*.js')
-        .pipe(webpack(config))
-        .pipe(gulp.dest('./www/js'))
-})
+  const webpack = require('webpack-stream');
+  const config = require('./webpack.config.js');
+  gulp.src('./src/js/**/*.js')
+    .pipe(webpack(config))
+    .pipe(gulp.dest('./www/js'));
+});
 
 // 编译less
 gulp.task('less', () => {
-    console.log('less')
-    const less = require('gulp-less')
+  console.log('less');
+  const less = require('gulp-less');
 
-    gulp.src('./src/less/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./www/css'))
-})
+  gulp.src('./src/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./www/css'));
+});
 
-
-gulp.task('default',  ['webpack', 'less'])
+gulp.task('default', ['webpack', 'less']);
 gulp.task('watch', () => {
-    gulp.watch('src/less/**/*.less', ['less'])
-    gulp.watch('src/js/**/*.js', ['webpack'])
-})
+  gulp.watch('src/less/**/*.less', ['less']);
+  gulp.watch('src/js/**/*.js', ['webpack']);
+});
---
+++ src/js/core/generator.js
@@ -0,0 +1,73 @@
+/**
+ * 生成数独解决方案
+ */
+const Toolkit = require('./toolkit');
+
+class Generator {
+  generate() {
+    while (!this.internalGenerate()) {
+      //
+    }
+  }
+
+  internalGenerate() {
+    this.matrix = Toolkit.matrix.makeMatrix();
+    this.orders = Toolkit.matrix
+      .makeMatrix()
+      .map(row => row.map((v, i) => i))
+      .map(row => Toolkit.matrix.shuffle(row));
+
+    for (let n = 1; n <= 9; n++) {
+      if (!this.fillNumber(n)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  fillNumber(n) {
+    return this.fillRow(n, 0);
+  }
+
+  // 当前行填写n成功，递归调用 fillRow 来在下一行填写n
+  fillRow(n, rowIndex) {
+    if (rowIndex > 8) {
+      return true;
+    }
+
+    const row = this.matrix[rowIndex];
+    const orders = this.orders[rowIndex];
+
+    for (let i = 0; i < 9; i++) {
+      const colIndex = orders[i];
+      // 如果这个位置有值，跳过
+      if (row[colIndex]) {
+        continue;
+      }
+
+      // 检查这个位置是否可以填n
+      if (!Toolkit.matrix.checkFillable(this.matrix, n, rowIndex, colIndex)) {
+        continue;
+      }
+
+      row[colIndex] = n;
+
+      // 去下一行填写n, 如果没填进去，就继续寻找当前行的下一个位置
+      if (!this.fillRow(n, rowIndex + 1)) {
+        row[colIndex] = 0;
+        continue;
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+}
+
+module.exports = Generator;
+
+const generator = new Generator();
+generator.generate();
+console.log('-----------------generator.matrix---------------');
+console.log(generator.matrix);
